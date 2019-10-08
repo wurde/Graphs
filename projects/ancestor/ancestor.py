d973e267cd0a6bcef68ca504a91c23fd185431e5
Andy Bettisworth <andy@accreu.com>
2019-10-08 16:02:29 -0500

    Define graph helper

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,36 @@
+#
+# Define data structures
+#
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+        # self.vertices = {
+        #   3: {1,2},
+        #   6: {3,5},
+        #   7: {5},
+        #   5: {4},
+        #   8: {4,11},
+        #   9: {8},
+        #   1: {10},
+        # }
+    
+    def add_vertex(self, parent, child):
+        if child not in self.vertices:
+            self.vertices[child] = set()
+
+        self.vertices[child].add(parent)
+
+#
+# Define search utility
+#
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    for relationship in ancestors:
+        graph.add_vertex(relationship[0], relationship[1])
+
+    print(f"graph.vertices {graph.vertices}")
+    #=> {3: {1, 2}, 6: {3, 5}, 7: {5}, 5: {4}, 8: {11, 4}, 9: {8}, 1: {10}}
+    
